5893b4a81a1f43100cdd1c77d4c2c10e13d3f7e4
Privat <privat@Ayhans-MacBook-Pro.local>
2020-09-26 12:06:03 +0200

    Added globals for reusable base domains across the configs

--- pizzapy/urls.py
+++ pizzapy/urls.py
@@ -1,5 +1,7 @@
 COUNTRY_USA = 'us'
 COUNTRY_CANADA = 'ca'
+DOMAIN_BASE_US = 'https://order.dominos.com/'
+DOMAIN_BASE_CA = 'https://order.dominos.ca/'
 
 class Urls(object):
     """URLs for doing different things to the API.
@@ -14,26 +16,26 @@ class Urls(object):
         self.country = country
         self.urls = {
             COUNTRY_USA: {
-                'find_url' : 'https://order.dominos.com/power/store-locator?s={line1}&c={line2}&type={type}',
-                'info_url' : 'https://order.dominos.com/power/store/{store_id}/profile',
-                'menu_url' : 'https://order.dominos.com/power/store/{store_id}/menu?lang={lang}&structured=true',
-                'place_url' : 'https://order.dominos.com/power/place-order',
-                'price_url' : 'https://order.dominos.com/power/price-order',
+                'find_url' : f'{DOMAIN_BASE_US}store-locator?s={line1}&c={line2}&type={type}',
+                'info_url' : f'{DOMAIN_BASE_US}store/{store_id}/profile',
+                'menu_url' : f'{DOMAIN_BASE_US}power/store/{store_id}/menu?lang={lang}&structured=true',
+                'place_url' : f'{DOMAIN_BASE_US}power/place-order',
+                'price_url' : f'{DOMAIN_BASE_US}power/price-order',
                 'track_by_order' : 'https://trkweb.dominos.com/orderstorage/GetTrackerData?StoreID={store_id}&OrderKey={order_key}',
                 'track_by_phone' : 'https://trkweb.dominos.com/orderstorage/GetTrackerData?Phone={phone}',
-                'validate_url' : 'https://order.dominos.com/power/validate-order',
-                'coupon_url' : 'https://order.dominos.com/power/store/{store_id}/coupon/{couponid}?lang={lang}',
+                'validate_url' : f'{DOMAIN_BASE_US}power/validate-order',
+                'coupon_url' : f'{DOMAIN_BASE_US}power/store/{store_id}/coupon/{couponid}?lang={lang}',
             },
             COUNTRY_CANADA: {
-                'find_url' : 'https://order.dominos.ca/power/store-locator?s={line1}&c={line2}&type={type}',
-                'info_url' : 'https://order.dominos.ca/power/store/{store_id}/profile',
-                'menu_url' : 'https://order.dominos.ca/power/store/{store_id}/menu?lang={lang}&structured=true',
-                'place_url' : 'https://order.dominos.ca/power/place-order',
-                'price_url' : 'https://order.dominos.ca/power/price-order',
+                'find_url' : f'{DOMAIN_BASE_CA}power/store-locator?s={line1}&c={line2}&type={type}',
+                'info_url' : f'{DOMAIN_BASE_CA}power/store/{store_id}/profile',
+                'menu_url' : f'{DOMAIN_BASE_CA}power/store/{store_id}/menu?lang={lang}&structured=true',
+                'place_url' : f'{DOMAIN_BASE_CA}power/place-order',
+                'price_url' : f'{DOMAIN_BASE_CA}power/price-order',
                 'track_by_order' : 'https://trkweb.dominos.ca/orderstorage/GetTrackerData?StoreID={store_id}&OrderKey={order_key}',
                 'track_by_phone' : 'https://trkweb.dominos.ca/orderstorage/GetTrackerData?Phone={phone}',
-                'validate_url' : 'https://order.dominos.ca/power/validate-order',
-                'coupon_url' : 'https://order.dominos.ca/power/store/{store_id}/coupon/{couponid}?lang={lang}',
+                'validate_url' : f'{DOMAIN_BASE_CA}power/validate-order',
+                'coupon_url' : f'{DOMAIN_BASE_CA}power/store/{store_id}/coupon/{couponid}?lang={lang}',
             }
         }
     
